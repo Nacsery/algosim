1f23f30b6499669448fccbb2b5c6d5ed693e8731
Nacsery <hp_bs@outlook.com>
2021-04-07 20:55:44 +0300

    bg colour

--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container } from 'react-bootstrap';
-
+document.body.style.backgroundColor = "#0d6efd";
 ReactDOM.render(
   <React.StrictMode >
-    <Container style={{
-      height: '100vh'
-    }}
-      className="bg-primary" fluid>
+    <Container fluid>
       <Container style={{
         paddingTop: 10
       }} fluid>
