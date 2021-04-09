8921a786e4572cd8af6920e1402cd4c08f362545
Nacsery <hp_bs@outlook.com>
2021-04-09 15:47:59 +0300

    fixed a key warning

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,17 +1,13 @@
 import { Button, Container } from 'react-bootstrap';
-
+import React from 'react';
 //Maps the button data to actual buttons
 //Render the buttons
 export default function Buttons(props) {
-    const keyList = props.buttons.map(button => (
-        button.key
-    ));
-    console.log(keyList);
     const buttonList = props.buttons.map(button => (
-        <>
+        <React.Fragment key={button.key}>
             {' '}
             <Button as="input" type="button" size="sm" value={button.name} onClick={button.event} key={button.key} />
-        </>
+        </React.Fragment>
     ));
 
     return (
